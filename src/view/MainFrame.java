a7f12d4965785924fea0d1dd8c305ce5109c8acb
Adam Danielczyk <adam.danielczyk@performgroup.com>
2017-01-03 23:08:03 +0100

    Add Jframe tittle

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -31,6 +31,7 @@ public class MainFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public MainFrame() {
+		setTitle("PRZYCHODNIA");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
